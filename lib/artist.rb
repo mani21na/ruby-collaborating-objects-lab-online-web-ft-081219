5220403220bcf4bca731d9e3783c9f0e2e700019
Hanna Lee <mani21na@gmail.com>
2019-08-27 21:07:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,12 +5,13 @@ class Artist
 
    def initialize(name)
     @name = name
-    @songs= []
+    #@songs= []
     save
   end
 
    def add_song(song)
-    @songs << song
+     new_song = Song.new(song)
+    #@songs << song
     song.artist = self
   end
 
