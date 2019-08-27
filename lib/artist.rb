473f834df581f851cc49d1f772de004667fb02f3
Hanna Lee <mani21na@gmail.com>
2019-08-27 20:24:02 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist 
+  attr_accessor = :name, :songs 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def name=(name)
+    @name = name
+  end  
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(title)
+    self.all << title
+    title.name = self
+  end  
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+end 
