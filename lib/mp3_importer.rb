6201c5e27f194522741fd0117bed5b36f071f766
Hanna Lee <mani21na@gmail.com>
2019-08-27 20:40:32 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,12 @@
 class MP3Importer 
-  attr_accessor :path 
+  attr_accessor :path, @files 
   
   def initialize(music_path)
     @path = music_path
   end
   
   def files
-    files = []
+    @files = []
     files = Dir["#{@music_path}/*.mp3"]
     files
   end
